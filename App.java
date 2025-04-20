cd091f2f965139683ea1bea4a791e8515ff0a93c
root <root@localhost.localdomain>
2025-04-20 15:14:11 +0530

    Initial commit - Hello Jenkins Project

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
+
+    public static String greet() {
+        return "Hello from Jenkins!";
+    }
+}
+
